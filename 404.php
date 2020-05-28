e52d1891788fce52b69d4119d2384c95660d762f
kanyeceejayA <33685273+kanyeceejayA@users.noreply.github.com>
2020-05-28 08:56:37 +0300

    added 404

---
+++ 404.php
@@ -0,0 +1,62 @@
+<?php
+    $pageName='404';
+    include('header.php');
+?>
+
+                <ul class="social social-rounded social-rounded-fixed social-rounded-dark">
+                  <li>
+                      <a href="https://www.linkedin.com/company/rootless-technologies-limited/"  target="_blank" rel="nofollow">
+                          <i class="socicon-linkedin"></i>
+                      </a>
+                  </li>
+                    <li>
+                        <a href="https://twitter.com/kanyeceejay"  target="_blank" rel="nofollow">
+                            <i class="socicon-twitter"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.facebook.com/rtlug/"  target="_blank" rel="nofollow">
+                            <i class="socicon-facebook"></i>
+                        </a>
+                    </li>
+                </ul>
+                <div class="establised">establised 2012</div>
+                <div class="copyright-fixed">&copy; RTL <script>  document.write((new Date()).getFullYear());</script>  &nbsp;</div>
+                <div class="home-piling a-pagepiling full-height">
+
+                    <div class="section pp-scrollable slide slide2">
+                        <div class="slide-container">
+                            <div class="slide-bg slide-bg-circle">
+                                <div class="inside"></div>
+                            </div>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="solution-num text-texture">404</div>
+                                        <div class="solution-num-title">
+                                            Page not Found
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <h2 class="slide-title font-libre">
+                                          Sorry,<br/>
+                                          We've failed to find what you're looking for
+                                        </h2>
+                                        <div id="solutions">
+                                            <p>Try retyping the link, or, alternatively, check out our homepage.</p>
+                                        </div>
+                                        <a class="btn btn-primary btn-rounded mr-4" href="index" class="animsition-link a-change-bg" data-animsition-out-class="fade-out" data-text="Home">
+                                            Home <i class="icon ion-ios-arrow-right"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                
+<?php include('footer.php');?>
+<script>
+    $('body').removeClass('dark-horizontal');
+    </script>
