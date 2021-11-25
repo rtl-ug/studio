802787870c52a79cd87d5f13a9a203bff189b173
kanyeceejayA <kakbr800@gmail.com>
2021-11-25 18:55:34 +0200

    additions yahss

---
+++ yah-privacy-policy.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf-8'>
+<meta name='viewport' content='width=device-width'>
+<title>Privacy Policy</title>
+<style> body { font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; padding:1em; }p{color: #66727f;} </style>
+</head>
+<body>
+<h1>Youth Action Handbook App Privacy Policy</h1>
+<div class="field field-body field-label-hidden field-item">
+    <p>Great Lakes Youth Network for Dialogue and Peace built the Youth Action Handbook App app as a Free app. </p>
+    
+    <p>This Service is provided by Great Lakes Youth Network for Dialogue and Peace at no cost and is intended for use as is. </p>
+    
+    <p>This page is used to inform visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use this Service. </p>
+    
+    <p>If you choose to use this Service, then you agree to the collection and use of information in relation to this policy.</p>
+    
+    <p>The Personal Information that we collect is used for providing and improving the Service. </p>
+    
+    <p>We will not use or share your information with anyone except as described in this Privacy Policy. </p>
+    
+    <p>The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at Youth Action Handbook App unless otherwise defined in this Privacy Policy.</p>
+
+    <p><strong>Information Collection and Use </strong></p>
+    
+    <p>For a better experience, while using this Service, we may require you to provide us with certain personally identifiable information. </p>
+    
+    <p>The information that we request will be retained by us and used as described in this privacy policy. </p>
+    
+    <p>The app does use third party services that may collect information used to identify you. </p>
+    
+    <p>Link to privacy policy of third party service providers used by the app</p>
+    
+    <ul>
+        <li><a href="https://www.google.com/policies/privacy/" target="_blank" rel="noopener">Google Play Services</a></li>
+        <li><a href="https://firebase.google.com/policies/analytics" target="_blank" rel="noopener">Google Analytics for Firebase</a></li>
+        <li><a href="https://firebase.google.com/support/privacy/" target="_blank" rel="noopener">Firebase Crashlytics</a></li>
+    </ul>
+    
+    <p><strong>Log Data</strong></p>
+    
+    <p>We want to inform you that whenever you use this Service, in a case of an error in the app we collect data and information (through third party products) on your phone called Log Data. </p>
+    
+    <p>This Log Data may include information such as your device Internet Protocol ("IP") address, device name, operating system version, the configuration of the app when utilizing this Service, the time and date of your use of the Service, and other statistics.</p>
+    
+    <p><strong>Cookies</strong></p>
+    
+    <p>Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. </p>
+    
+    <p>These are sent to your browser from the websites that you visit and are stored on your device's internal memory. </p>
+    
+    <p>This Service does not use these "cookies" explicitly. </p>
+    
+    <p>However, the app may use third party code and libraries that use "cookies" to collect information and improve their services. </p>
+    
+    <p>You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. </p>
+    
+    <p>If you choose to refuse our cookies, you may not be able to use some portions of this Service.</p>
+    
+    <p><strong>Service Providers </strong></p>
+    
+    <p>We may employ third-party companies and individuals due to the following reasons:</p>
+    
+    <ul>
+        <li>To facilitate this Service;</li>
+        <li>To provide the Service on our behalf;</li>
+        <li>To perform Service-related services; or</li>
+        <li>To assist us in analyzing how this Service is used.</li>
+    </ul>
+    
+    <p>We want to inform users of this Service that these third parties have access to your Personal Information. </p>
+    
+    <p>The reason is to perform the tasks assigned to them on our behalf. </p>
+    
+    <p>However, they are obligated not to disclose or use the information for any other purpose.</p>
+    
+    <p><strong>Security </strong></p>
+    
+    <p>We value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. </p>
+    
+    <p>But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.</p>
+    
+    <p><strong>Links to Other Sites</strong></p>
+    
+    <p>This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. </p>
+    
+    <p>Note that these external sites are not operated by us. </p>
+    
+    <p>Therefore, we strongly advise you to review the Privacy Policy of these websites. </p>
+    
+    <p>We have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.</p>
+    
+    <p><strong>Children’s Privacy</strong></p>
+    
+    <p>These Services do not address anyone under the age of 13. </p>
+    
+    <p>We do not knowingly collect personally identifiable information from any of our users, including children. </p>
+    
+    <p>Changes to This Privacy Policy We may update our Privacy Policy from time to time. </p>
+    
+    <p>Thus, you are advised to review this page periodically for any changes. </p>
+    
+    <p>We will notify you of any changes by posting the new Privacy Policy on this page. </p>
+    
+    <p>This policy is effective as of 2020-12-29</p>
+    
+    <p><strong>Contact Us</strong></p>
+    
+    <p>If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us at .</p>
+    
+    <p><a href="mailto:info@Youth Action Handbook App.go.ug?subject=Mail from Youth Action Handbook App Site">info@Youth Action Handbook App.go.ug</a></p>
+    </div>
+ </body>
+ </html>
